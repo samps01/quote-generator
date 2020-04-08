c519bd5b724945cb7841ff0b71467088ef7bdf9e
samson pallivathukkal <samps01@gmail.com>
2020-04-08 01:28:26 -0400

    Change UI design and add bg image change logic

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('App.js loaded');
+let imageIndex = 0; // Initiate with 0
+
+/**
+ * Method called on click event of the generate button
+ */
+function generateQuote() {
+    bgImageGenerator();
+}
+
+/**
+ * Method to generate random images for background
+ * @return {void}
+ */
+function bgImageGenerator() {
+    let bgImageElement = document.querySelector('#bg-image');
+    let bgImageUrl = ['assets/img/image1.jpeg', 'assets/img/image2.jpeg', 'assets/img/image3.jpeg'];
+
+    if (imageIndex < bgImageUrl.length - 1) {
+        imageIndex++;
+    } else {
+        imageIndex = 0;
+    }
+
+    bgImageElement.setAttribute('style', `background-image: url(${bgImageUrl[imageIndex]})`);
+}
