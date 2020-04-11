1f7e086e55f1958c2ce57b30e40d64a8309b65d5
samson pallivathukkal <samps01@gmail.com>
2020-04-11 01:17:24 -0400

    Add gradient to to bg image

--- app.js
+++ app.js
@@ -8,19 +8,32 @@ function generateQuote() {
 }
 
 /**
+ * Method to generate Random number based on max and min
+ * @param maxValue
+ * @param minValue
+ * @returns {number}
+ */
+function generateRandomNumber(maxValue, minValue) {
+    try {
+        if (typeof maxValue === "number" && typeof minValue === "number" && maxValue > minValue) {
+            return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+        } else {
+            throw "Invalid parameter provided";
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+/**
  * Method to generate random images for background
  * @return {void}
  */
 function bgImageGenerator() {
     let bgImageElement = document.querySelector('#bg-image');
     let bgImageUrl = ['assets/img/image1.jpeg', 'assets/img/image2.jpeg', 'assets/img/image3.jpeg', 'assets/img/image4.jpg', 'assets/img/image5.jpg', 'assets/img/image6.jpg', 'assets/img/image7.jpg', 'assets/img/image8.jpg'];
-
-    let imageIndex = Math.floor(Math.random() * (7 - 0 + 1) + 0);
-    /*if (imageIndex < bgImageUrl.length - 1) {
-        imageIndex++;
-    } else {
-        imageIndex = 0;
-    }*/
-
+    let imageIndex = generateRandomNumber(bgImageUrl.length - 1, 0);
     bgImageElement.setAttribute('style', `background-image: url(${bgImageUrl[imageIndex]})`);
 }
+
+
