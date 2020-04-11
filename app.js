55c9fa334eb9af059242e6f4abc7d47b1b5fbff7
benzps01 <benzps01@gmail.com>
2020-04-11 09:27:48 +0530

    Randomizing image

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let imageIndex = 0; // Initiate with 0
+//let imageIndex = 0; // Initiate with 0
 
 /**
  * Method called on click event of the generate button
@@ -13,13 +13,14 @@ function generateQuote() {
  */
 function bgImageGenerator() {
     let bgImageElement = document.querySelector('#bg-image');
-    let bgImageUrl = ['assets/img/image1.jpeg', 'assets/img/image2.jpeg', 'assets/img/image3.jpeg'];
+    let bgImageUrl = ['assets/img/image1.jpeg', 'assets/img/image2.jpeg', 'assets/img/image3.jpeg', 'assets/img/image4.jpg', 'assets/img/image5.jpg', 'assets/img/image6.jpg', 'assets/img/image7.jpg', 'assets/img/image8.jpg'];
 
-    if (imageIndex < bgImageUrl.length - 1) {
+    let imageIndex = Math.floor(Math.random() * (7 - 0 + 1) + 0);
+    /*if (imageIndex < bgImageUrl.length - 1) {
         imageIndex++;
     } else {
         imageIndex = 0;
-    }
+    }*/
 
     bgImageElement.setAttribute('style', `background-image: url(${bgImageUrl[imageIndex]})`);
 }
